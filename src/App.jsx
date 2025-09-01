44ae357bbb7ebe395aa3d63cd6cf48446e932faa
Akash Bisht <56999019+AKASH-18@users.noreply.github.com>
2025-09-01 22:38:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <button className="redirect">
+        <a href="https://netflix-gen-dfemm9o36-akash-18s-projects.vercel.app/">
+          CLICK HERE TO PROJECT
+        </a>
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
